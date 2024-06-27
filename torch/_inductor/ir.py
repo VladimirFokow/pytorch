7374f3570188f90826240dc18c5a1d656509e166
Yueming Hao <yhao@meta.com>
2024-06-27 11:16:53 -0700

    fix layout error; remove comments

--- torch/_inductor/ir.py
+++ torch/_inductor/ir.py
@@ -1839,7 +1839,7 @@ def as_storage_and_layout(
         return buffer, x.layout
     if isinstance(x, DtypeView):
         buffer, _ = as_storage_and_layout(x.data, freeze=freeze)
-        return buffer, x.data.layout
+        return buffer, x.layout
     raise NotImplementedError
 
 
